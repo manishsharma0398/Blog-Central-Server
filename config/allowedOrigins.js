435a332b42cc9180c83bf9406b65396b320a6a62
Manish Sharma <manish.sharma0398@gmail.com>
2023-02-19 18:42:12 +0530

    made changes to allowedOrigins file for cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,12 @@
 const allowedOrigins = [
   "https://blog-central-client.vercel.app",
-  "blog-central-client-3y5041ryb-manishsharma0398.vercel.app",
+  "https://blog-central-client.vercel.app/",
+  "https://blog-central-client-4j003j7bn-manishsharma0398.vercel.app",
+  "https://blog-central-client-4j003j7bn-manishsharma0398.vercel.app/",
   "https://blog-central-client-manishsharma0398.vercel.app",
+  "https://blog-central-client-manishsharma0398.vercel.app/",
   "https://blog-central-client-git-main-manishsharma0398.vercel.app",
-  "https://blog-central-client-3y5041ryb-manishsharma0398.vercel.app",
+  "https://blog-central-client-git-main-manishsharma0398.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
