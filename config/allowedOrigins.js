c01a3c32b8eb4eb5d9b0946c3befb7aa3c2d96c2
Manish Sharma <manish.sharma0398@gmail.com>
2023-02-19 18:12:36 +0530

    mad echanges for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,9 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = [
+  "https://blog-central-client.vercel.app",
+  "blog-central-client-3y5041ryb-manishsharma0398.vercel.app",
+  "https://blog-central-client-manishsharma0398.vercel.app",
+  "https://blog-central-client-git-main-manishsharma0398.vercel.app",
+  "https://blog-central-client-3y5041ryb-manishsharma0398.vercel.app",
+];
 
 module.exports = allowedOrigins;
