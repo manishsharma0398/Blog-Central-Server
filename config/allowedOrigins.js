1b1bcd63cc11ba305454ef573994b69cc481210b
Manish Sharma <manish.sharma0398@gmail.com>
2023-03-07 22:53:40 +0530

    updated allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,16 @@
 const allowedOrigins = [
   "http://localhost:5173",
+  "blog-central-client.vercel.app",
   "https://blog-central-client.vercel.app",
+  "blog-central-client.vercel.app/",
   "https://blog-central-client.vercel.app/",
-  "https://blog-central-client-4j003j7bn-manishsharma0398.vercel.app",
-  "https://blog-central-client-4j003j7bn-manishsharma0398.vercel.app/",
+  "blog-central-client-manishsharma0398.vercel.app",
   "https://blog-central-client-manishsharma0398.vercel.app",
+  "blog-central-client-manishsharma0398.vercel.app/",
   "https://blog-central-client-manishsharma0398.vercel.app/",
+  "blog-central-client-git-main-manishsharma0398.vercel.app",
   "https://blog-central-client-git-main-manishsharma0398.vercel.app",
+  "blog-central-client-git-main-manishsharma0398.vercel.app/",
   "https://blog-central-client-git-main-manishsharma0398.vercel.app/",
 ];
 
